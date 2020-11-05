781229091e87e1e4bc594b0fd87e09176bfe16fc
OliverBuez <72299052+OliverBuez@users.noreply.github.com>
2020-11-05 11:39:44 +0100

    Update sulfuras.py

--- model/sulfuras.py
+++ model/sulfuras.py
@@ -1,7 +1,8 @@
 from .abstractitem import AbstractItem
 
 class Sulfuras(AbstractItem):
-    def __init__(self, name, sell_in, _=None):
+    def __init__(self, name, sell_in, quality=None):
+        super.__init__(name, sell_in, quality)
         self.name = name
         self.sell_in = sell_in
         self.quality = 80
