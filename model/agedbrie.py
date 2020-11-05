756f94f7f021432f61dcb9f72062c5077756653d
OliverBuez <72299052+OliverBuez@users.noreply.github.com>
2020-11-05 11:36:33 +0100

    Update agedbrie.py

--- model/agedbrie.py
+++ model/agedbrie.py
@@ -4,5 +4,5 @@ class AgedBrie(AbstractItem):
 
     def _update_quality(self):
         if self.sell_in <= 0:
-            return self._decrease_quality(1)
-        return self._increase_quality(1)
+            return self._decrease_quality(by=1)
+        return self._increase_quality(by=1)
